4e8aa8b603303ea78614452bc3809144ba74fa4a
akashraw <aakkicoool@gmail.com>
2022-08-04 13:39:01 +0530

    w

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,10 +7,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
   var: any;
-  PHP_API_SERVER = "https://www.tripgusto.com";
+  API_SERVER = "https://z44go7o5dunqz2x4cw6htezlo40pykjd.lambda-url.ap-south-1.on.aws/";
 	constructor(private httpClient: HttpClient) {}
 	createQuery(query: CData): Observable<CData>{
-		return this.var= this.httpClient.post<CData>(`${this.PHP_API_SERVER}/backend/create.php`, query)
+		return this.var= this.httpClient.post<CData>(this.API_SERVER, query)
 		   
 	}
 	
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-
     apiKey: "AIzaSyDdcftcBOO7ROX-Bu4H7BIM3E4KyYrMmtU",
-  
     authDomain: "tripgustodata.firebaseapp.com",
-  
+    databaseURL: "https://tripgustodata-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "tripgustodata",
-  
     storageBucket: "tripgustodata.appspot.com",
-  
     messagingSenderId: "1007193081472",
-  
-    appId: "1:1007193081472:web:70432ddd678043758bc865",
-  
-    measurementId: "G-RR33BBN4LD"
+    appId: "1:1007193081472:web:e329767fd07232528bc865",
+    measurementId: "G-6ET79W6E3P"
   
   }
 };
